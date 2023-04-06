0892a197f0456ef794994fd9cb7eff53b312bbb5
lamxuanthinh <thinhlam749@gmail.com>
2023-04-06 15:16:17 +0700

    remove main and bootstrap day 01

--- app/src/main/java/com/example/project03/EmployeeModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.project03
-
-data class EmployeeModel(
-    val empId:String? = null,
-    val empName:String? = null,
-    val empAge:String? = null,
-    val empDescription:String? = null,
-)
--- app/src/main/java/com/example/project03/MainActivity.kt
+++ app/src/main/java/com/example/project03/MainActivity.kt
@@ -9,7 +9,6 @@ import com.google.firebase.database.FirebaseDatabase
 
 
 private lateinit var binding: ActivityMainBinding
-private lateinit var dbRef: DatabaseReference
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,31 +16,5 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        // Test connection database with Firebase
-        dbRef = FirebaseDatabase.getInstance().getReference("Employees")
-
-        binding.btnAdd.setOnClickListener {
-            saveEmployeesData()
-        }
-    }
-
-    private fun saveEmployeesData() {
-        val empId = dbRef.push().key!!
-        val empName = binding.edtName.text.toString()
-        val empAge = binding.edtAge.text.toString()
-        val empDescription = binding.edtDescription.text.toString()
-
-        binding.edtName.setText("")
-        binding.edtAge.setText("")
-        binding.edtDescription.setText("")
-
-        val employee = EmployeeModel(empId, empName, empAge, empDescription)
-
-        dbRef.child(empId).setValue(employee).addOnCompleteListener {
-                Toast.makeText(this, "successfully connected", Toast.LENGTH_LONG).show()
-            }.addOnFailureListener { error ->
-                Toast.makeText(this, "Fail connected ${error.message}", Toast.LENGTH_LONG).show()
-            }
     }
 }
