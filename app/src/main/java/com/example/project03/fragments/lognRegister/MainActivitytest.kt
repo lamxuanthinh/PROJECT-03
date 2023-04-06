9ef94712880dceb46231957103bf317b4ffcbfe5
lamxuanthinh <thinhlam749@gmail.com>
2023-04-06 16:38:35 +0700

    log

---
+++ app/src/main/java/com/example/project03/fragments/lognRegister/MainActivitytest.kt
@@ -0,0 +1,11 @@
+package com.example.project03.fragments.lognRegister
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivitytest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activitytest)
+    }
+}
