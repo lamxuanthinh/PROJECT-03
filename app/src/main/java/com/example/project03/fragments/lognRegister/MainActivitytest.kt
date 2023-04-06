6be08428705dd8d94a348e0fd988a5e72c90617b
lamxuanthinh <thinhlam749@gmail.com>
2023-04-06 16:39:34 +0700

    lost

--- app/src/main/java/com/example/project03/fragments/lognRegister/MainActivitytest.kt
+++ app/src/main/java/com/example/project03/fragments/lognRegister/MainActivitytest.kt
@@ -2,6 +2,7 @@ package com.example.project03.fragments.lognRegister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.project03.R
 
 class MainActivitytest : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
