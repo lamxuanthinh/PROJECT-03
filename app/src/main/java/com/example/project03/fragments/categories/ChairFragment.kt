e57634b0123df361daabde0ccfee1d884479f38c
AoiChang123 <trangtran1082k3@gmail.com>
2023-04-09 17:59:19 +0700

    #12 UI TabLayout

---
+++ app/src/main/java/com/example/project03/fragments/categories/ChairFragment.kt
@@ -0,0 +1,4 @@
+package com.example.project03.fragments.categories
+
+class ChairFragment: BaseCategoryFragment() {
+}
---
+++ app/src/main/java/com/example/project03/fragments/categories/MainCategoryFragment.kt
@@ -0,0 +1,7 @@
+package com.example.project03.fragments.categories
+
+import androidx.fragment.app.Fragment
+import com.example.project03.R
+
+class MainCategoryFragment: Fragment(R.layout.fragment_main_category) {
+}
