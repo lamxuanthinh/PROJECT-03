74b8f1ec7627c7a0ba578ff379f6d359ec71c623
damthilan <damthilan2020@gmail.com>
2023-04-11 08:29:50 +0700

    updata #11

--- app/src/main/java/com/example/project03/fragments/categories/MainCategory.kt
+++
@@ -1,8 +0,0 @@
-package com.example.project03.fragments.categories
-
-import androidx.fragment.app.Fragment
-import com.example.project03.R
-
-class MainCategory:Fragment(R.layout.fragment_main_category) {
-
-}
