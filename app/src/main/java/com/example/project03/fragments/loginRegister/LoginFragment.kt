d639efe406b25d605dbada5b5deb6d38dff3a842
lamxuanthinh <thinhlam749@gmail.com>
2023-04-14 22:39:25 +0700

    handle authentication

--- app/src/main/java/com/example/project03/fragments/loginRegister/LoginFragment.kt
+++ app/src/main/java/com/example/project03/fragments/loginRegister/LoginFragment.kt
@@ -1,13 +1,7 @@
 package com.example.project03.fragments.loginRegister
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.project03.R
 
+import androidx.fragment.app.Fragment
 
 class LoginFragment : Fragment() {
-
 }
---
+++ app/src/main/java/com/example/project03/util/ValidationCheck.kt
@@ -0,0 +1,21 @@
+package com.example.project03.util
+
+import android.util.Patterns
+
+
+fun validateEmail(email: String): RegisterValidation {
+    if (email.isEmpty()) return RegisterValidation.Failed("Email can not be empty")
+
+    if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) return RegisterValidation.Failed("Wrong email format")
+
+    return RegisterValidation.Success
+}
+
+fun validatePassword(password: String): RegisterValidation {
+    if (password.isEmpty()) return RegisterValidation.Failed("Password can not be empty")
+
+    if (password.length < 6) return RegisterValidation.Failed("Password should contain 6 char")
+
+    return RegisterValidation.Success
+}
+
