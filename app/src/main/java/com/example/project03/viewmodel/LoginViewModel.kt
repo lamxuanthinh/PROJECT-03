5c744730023fe9250fa14a69eb6e56d721446a30
lamxuanthinh <thinhlam749@gmail.com>
2023-04-17 08:56:50 +0700

    reset password

--- app/src/main/java/com/example/project03/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/project03/viewmodel/LoginViewModel.kt
@@ -18,6 +18,9 @@ class LoginViewModel @Inject constructor(
     private val _login = MutableSharedFlow<Resource<FirebaseUser>>()
     val login = _login.asSharedFlow()
 
+    private val _resetPassword = MutableSharedFlow<Resource<String>>()
+    val resetPassword = _resetPassword.asSharedFlow()
+
     fun login(email: String, password: String) {
         viewModelScope.launch { _login.emit(Resource.Loading()) }
         firebaseAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
@@ -29,6 +32,21 @@ class LoginViewModel @Inject constructor(
         }.addOnFailureListener {
             viewModelScope.launch {
                 _login.emit(Resource.Error(it.message.toString()))
+            }
+        }
+    }
+
+    fun resetPassword(email: String) {
+        viewModelScope.launch {
+            _resetPassword.emit(Resource.Loading())
+        }
+        firebaseAuth.sendPasswordResetEmail(email).addOnSuccessListener {
+            viewModelScope.launch {
+                _resetPassword.emit(Resource.Success(email))
+            }
+        }.addOnFailureListener {
+            viewModelScope.launch {
+                _resetPassword.emit(Resource.Error(it.message.toString()))
             }
         }
     }
