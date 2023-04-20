93dd47904258716e7cc978ba036182ac95af2c00
damthilan <damthilan2020@gmail.com>
2023-04-20 09:26:42 +0700

    #18 BaseCategory_setup

--- app/src/main/java/com/example/project03/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/example/project03/fragments/categories/BaseCategoryFragment.kt
@@ -5,15 +5,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.project03.R
+import com.example.project03.adapters.BestProductsAdapter
+import com.example.project03.databinding.FragmentBaseCateroryBinding
 
 open class BaseCategoryFragment: Fragment(R.layout.fragment_base_caterory) {
+    private lateinit var binding: FragmentBaseCateroryBinding
+    private lateinit var offerAdapter: BestProductsAdapter
+    private lateinit var bestProductsAdapter: BestProductsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentBaseCateroryBinding.inflate(inflater)
+        return binding.root
     }
-}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupOfferRv()
+        setupBestProductsRv()
+    }
+
+    private fun setupBestProductsRv() {
+        bestProductsAdapter = BestProductsAdapter()
+        binding.rvBestProducts.apply {
+            layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+            adapter = bestProductsAdapter
+
+        }
+    }
+
+    private fun setupOfferRv() {
+        offerAdapter = BestProductsAdapter()
+        binding.rvBestProducts.apply {
+            layoutManager = GridLayoutManager(requireContext(), 2, LinearLayoutManager.HORIZONTAL, false)
+            adapter = offerAdapter
+
+        }
+    }
+    }
