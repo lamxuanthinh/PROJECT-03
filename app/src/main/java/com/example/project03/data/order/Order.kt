5bade8e3e979dfef6db8bcfed686ca5604146cb9
damthilan <damthilan2020@gmail.com>
2023-04-22 14:30:54 +0700

    dfd

--- app/src/main/java/com/example/project03/data/order/Order.kt
+++ app/src/main/java/com/example/project03/data/order/Order.kt
@@ -1,4 +1,20 @@
 package com.example.project03.data.order
 
-class Order {
+import android.icu.text.SimpleDateFormat
+import com.example.project03.adapters.BestProductsAdapter
+import com.example.project03.data.Address
+import com.example.project03.data.CartProduct
+import java.util.*
+import kotlin.random.Random.Default.nextLong
+
+data class Order(
+    val orderStatus: String,
+    val totalPrice: String,
+    val products: List<CartProduct>,
+    val address: Address,
+    val date: String = SimpleDateFormat("YYYY-MM-dd", Locale.ENGLISH).format(Date()),
+    val orderId: Long = nextLong(0, 100_000_000_000) + totalPrice.toLong()
+
+
+) {
 }
--- app/src/main/java/com/example/project03/data/order/OrderStatus.kt
+++ app/src/main/java/com/example/project03/data/order/OrderStatus.kt
@@ -1,4 +1,30 @@
 package com.example.project03.data.order
 
-class OrderStatus {
+sealed class OrderStatus(val status: String) {
+    object Ordered: OrderStatus("Ordered")
+    object Canceled: OrderStatus("Canceled")
+    object Confirmed: OrderStatus("Confirmed")
+    object Shipped: OrderStatus("Shipped")
+    object Delivered: OrderStatus("Delivered")
+    object Returned: OrderStatus("Returned")
 }
+    fun getOderStatus(status: String): OrderStatus{
+        return when(status){
+            "Ordered" -> {
+                OrderStatus.Ordered
+            }
+            "Canceled" -> {
+                OrderStatus.Canceled
+            }
+            "Confirmed" -> {
+                OrderStatus.Confirmed
+            }
+            "Shipped" -> {
+                OrderStatus.Shipped
+            }
+            "Delivered" -> {
+                OrderStatus.Delivered
+            }
+            else -> OrderStatus.Returned
+        }
+    }
