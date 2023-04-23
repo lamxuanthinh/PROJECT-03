52728cd40136bbeda95822052976f972920b4208
AoiChang123 <trangtran1082k3@gmail.com>
2023-04-23 16:01:56 +0700

    #68 order detail

--- app/src/main/java/com/example/project03/data/order/Order.kt
+++ app/src/main/java/com/example/project03/data/order/Order.kt
@@ -1,13 +1,16 @@
 package com.example.project03.data.order
 
 
+import android.os.Parcelable
 import com.example.project03.data.Address
 import com.example.project03.data.CartProduct
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Order(
     val orderStatus: String,
     val totalPrice: Float,
     val products: List<CartProduct>,
     val address: Address
-)
+):Parcelable
 
